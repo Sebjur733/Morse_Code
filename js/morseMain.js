9e8f1aae115157f6b1c868ee61ceae8f63477706
Sebjur733 <258028@usn.no>
2026-02-18 20:03:36 +0100

    fixed sound

--- js/morseMain.js
+++ js/morseMain.js
@@ -10,8 +10,8 @@ let progressPosition = progressBox.getBoundingClientRect();
 console.log('position progressrect: ' + progressPosition.right);
 
 const borderLine = document.getElementById('border');
-let borderPostion = borderLine.getBoundingClientRect();
-console.log('position border: ' + borderPostion.right);
+
+
 
 let mosueDownTime = 0;
 let mouseUpTime = Date.now();
@@ -21,13 +21,14 @@ let progressInterval;
 
 let newProgressPosition = progressBox.getBoundingClientRect();
 
+let idleSpeed = 1000;
 let idleInterval = null;
 function startIdleTracking() {
   stopIdleTracking();
   idleInterval = setInterval(() => {
     MorsePrint.value += ' ';
     MorsePrint.scrollLeft = MorsePrint.scrollWidth;
-  }, 1000);
+  }, idleSpeed);
 }
 
 function stopIdleTracking() {
@@ -37,16 +38,44 @@ function stopIdleTracking() {
   }
 }
 
+
+
+
+let ctx, osc, gain;
+
+let mouseBool = false;
+//mouse down
 writeMorse.addEventListener('mousedown', function () {
-  document.getElementById('sound').play();
-  
+  mouseBool = true;
+   // Opprett AudioContext og oscillator første gang brukeren klikker
+    if (!ctx) {
+        ctx = new (window.AudioContext || window.webkitAudioContext)();
+        osc = ctx.createOscillator();
+        gain = ctx.createGain();
+
+        osc.frequency.value = 600;
+        osc.type = "sine";
+
+        gain.gain.value = 0;
+
+        osc.connect(gain);
+        gain.connect(ctx.destination);
+        osc.start();
+    }
+
+    // Resume AudioContext for å overstyre autoplay-policy
+    ctx.resume().then(() => {
+        gain.gain.setValueAtTime(1, ctx.currentTime);
+    });
+
+
  stopIdleTracking();
   mosueDownTime = Date.now();
 
   
   let progressPositionMove = 0;
 progressInterval = setInterval(() => {
-  console.log('position border: ' + borderPostion.right);
+  
   
   progressPositionMove ++;
   progressBox.style.width = `${progressPositionMove}px`;
@@ -55,15 +84,26 @@ progressInterval = setInterval(() => {
 }, 10);
 })
 
-writeMorse.addEventListener('mouseup', function () {
-  document.getElementById('sound').pause();
-  const duration = Date.now() - mosueDownTime;
+function checkPosition() {
+  let borderPostion = borderLine.getBoundingClientRect();
   if (newProgressPosition.right <= borderPostion.right) {
     MorsePrint.value += '.';
   }
   else {
     MorsePrint.value += '-';
   }
+}
+
+// mouse up
+addEventListener('mouseup', function () {
+  if (mouseBool === true) {
+    mouseBool = false;
+  if (gain) gain.gain.setValueAtTime(0, ctx.currentTime);
+
+  const duration = Date.now() - mosueDownTime;
+
+  checkPosition();
+  
   TextOutput.innerHTML = decodeMorse(MorsePrint.value);
   
 mouseUpTime = Date.now();
@@ -73,6 +113,7 @@ progressBox.style.width = '0px';
 MorsePrint.scrollLeft = MorsePrint.scrollWidth;
 
 startIdleTracking();
+  }
 });
 
 
